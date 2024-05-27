ee32af006a6fbaf027def16fdc78c03e97c1d475
Ping <ping15512@gmail.com>
2024-05-27 18:55:04 +0700

    add:cv upload

--- src/components/myCV.jsx
+++ src/components/myCV.jsx
@@ -1,6 +1,17 @@
 import "./myCV.css";
+import React from "react";
+import catImage from "../images/cat-3237903_1280.png";
 
 function MyCV() {
+  const downloadImage = () => {
+    const link = document.createElement("a");
+    link.href = catImage; // ใช้ URL ของไฟล์ภาพที่นำเข้า
+    link.download = "saterat-cv.png"; // ชื่อไฟล์ที่ต้องการให้ดาวน์โหลด
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <>
       <div className="myCV">
@@ -10,9 +21,10 @@ function MyCV() {
             src="/src/images/resume-2296951_1280.png"
             alt="cvPic"
           />
-          <button id="downlodeButton">download CV</button>
+          <button id="downlodeButton" onClick={downloadImage}>
+            Download CV
+          </button>
         </div>
-
         <img
           className="house"
           src="/src/images/neighborhood-6771319_1280.png"
